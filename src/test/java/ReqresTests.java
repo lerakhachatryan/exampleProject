66425d5f5851545978b90ca643536353e407b18f
Хачатрян Валерия <vkhachatryan@softrust.ru>
2024-03-01 12:09:38 +0300

    CRUD for user (homework)

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -6,6 +6,8 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
 
 public class ReqresTests {
+
+
     @Test
     void successfulLogin() {
         given()
@@ -34,11 +36,57 @@ public class ReqresTests {
     void createUser() {
         given()
                 .contentType(ContentType.JSON)
-                .body("{ \"name\": \"NiceTryBuddy\", \"job\": \"leader\" }")
+                .body("{ \"name\": \"nameofanotheruser\", \"job\": \"leader\"}")
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("nameofanotheruser"));
+    }
+
+    @Test
+    void getCreatedUser() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/12")
                 .then()
                 .statusCode(200)
-                .body("name", is("NiceTryBuddy"));
+                .body("name", is("nameofuser"));
+    }
+
+    @Test
+    void updateUserWithPut() {
+        given()
+                .contentType(ContentType.JSON)
+                //поменяем полностью тело
+                .body("{ \"name\": \"morpheuse\", \"job\": \"zion ex-resident\"}")
+                .when()
+                .put("https://reqres.in/api/users/12")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheuse"));
+    }
+
+    @Test
+    void updateUserWithPatch() {
+        given()
+                .contentType(ContentType.JSON)
+                //поменяем только имя
+                .body("{ \"name\": \"morpheus\", \"job\": \"zion ex-resident\"}")
+                .when()
+                .patch("https://reqres.in/api/users/12")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"));
+    }
+
+
+    @Test
+    void deleteUser() {
+        given()
+                .when()
+                .delete("https://reqres.in/api/users/12")
+                .then()
+                .statusCode(204);
     }
 }

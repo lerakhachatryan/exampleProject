0d23b1ad45fe9d2ef05bf1579b2ecd9839954eb2
Хачатрян Валерия <vkhachatryan@softrust.ru>
2024-03-06 18:55:50 +0300

    Добавляем проверку по AssertJ

--- src/test/java/ru/softrust/APITest.java
+++ src/test/java/ru/softrust/APITest.java
@@ -4,19 +4,35 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class APITest {
     @Test
     void checkTotal20() {
-        step("Открываем главную страницу", () -> {
+        step("Проверяем, что количество элементов равно 20", () -> {
         given()
                 .when()
                 .get("https://selenoid.autotests.cloud/status")
                 .then()
-                .body("total", is(22));
+                .body("total", is(20));
     });
     }
 
+    @Test
+    void checkTotal20WithAssertJ() {
+        step("Проверяем ответ с помощью AssertJ", () -> {
+        Integer response = get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract()
+                .path("total");
+
+        System.out.println(response);
+
+        assertThat(response).isEqualTo(20);
+    });
+    }
 }
+
